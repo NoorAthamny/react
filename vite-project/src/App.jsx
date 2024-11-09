b2af783fa465a49d387079691c73fd888a507337
NoorAthamny <athamnynoor@gmail.com>
2024-11-09 17:32:36 +0200

    birthday added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import data from "./assets/data";
+import List from "./List";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [people, setPeople] = useState(data);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthday Today </h3>
+        <List people={people} />
+        <button
+          onClick={() => setPeople([])}
+          type="button"
+          className="btn btn-block"
+        >
+          Clear List
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </section>
+    </main>
   );
 }
 
